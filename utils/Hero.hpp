fc735dc2f196e404d06ffc67e3c48ad225e62876
Nei tong <Hengneitong.keo@student.cadt.edu.kh>
2025-08-21 07:56:23 +0700

    Start code

--- utils/Hero.hpp
+++ utils/Hero.hpp
@@ -8,11 +8,7 @@ class Hero{
     private:
         // Define all the attributes here...
         // [ your code ]
-        string name;
-        string weapon;
-        int health;
-        int armor;
-        int power;
+        
         
     public:
         // Define the constructor and methods here ...
@@ -21,11 +17,7 @@ class Hero{
         {
             // assign each attributes' value from constructor param here...
             // [ your code ]
-            this->name = name;
-            this->weapon = weapon;
-            this->health = health;
-            this->armor = armor;
-            this->power = power;
+            
         }
 
         void setName(string name) { this->name = name; }
@@ -34,47 +26,14 @@ class Hero{
         void info(){
             // process display hero info here...
             // [ your code ]
-            cout << "Hero Name: " << name << ", Weapon: " << weapon << ", Health: " << health << ", Armor: " << armor << ", Power: " << power << endl;
-
-            cout << endl;
+            
         }
 
         void strike(Hero& enemy){
             // process the strike logic here...
             // you can add the narative text as well (optional)
             // [ your code ]
-            // int demage = power + (rand() % 10);
-            // int enemyDefence = enemy.getArmor();
-            // int actualDemage = max(demage, enemyDefence);
 
-            // enemy.health -= actualDemage;
-            // cout << name << " Attack " << enemy.name << " with " << weapon << "for " << actualDemage << " demage! " << endl;
-
-            // if (enemyDefence <= 0)
-            // {
-            //     cout << enemy.name << " has been slayed!!!" << endl;
-            // }
-
-            //std::cout << name << " attacks " << enemy.name << " with " << weapon << "!" << endl;
-
-            int damage = power;
-
-            if (enemy.armor >= damage) {
-                enemy.armor -= damage;
-                damage = 0;
-            } else {
-                damage -= enemy.armor;
-                enemy.armor = 0;
-            }
-
-            enemy.health -= damage;
-            if (enemy.health < 0) enemy.health = 0;
-
-            // std::cout << enemy.name << "'s remaining health: " << enemy.health
-            //         << ", remaining armor: " << enemy.armor << endl;
-
-            std::cout << name << " attacks " << enemy.name << endl;
-            std::cout << enemy.name << "'s armor should drop to " << enemy.armor << " and health to " << enemy.health << endl;
         }
 
         // getter methods
